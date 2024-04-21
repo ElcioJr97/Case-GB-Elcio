a6c34f4432f59bac8bb54593bc0f2aa9d9bc82fa
ElcioJr97 <elcio_jr97@hotmail.com>
2024-04-21 00:29:02 -0300

    tb_consolidado_vendas_ano_mes_sql
    alternativa de consulta a tabela consolidado vendas ano mes

---
+++ tb_consolidado_vendas_ano_mes_sql.sql
@@ -0,0 +1,10 @@
+SELECT 
+    EXTRACT(YEAR FROM DATA_VENDA) AS ANO,
+    EXTRACT(MONTH FROM DATA_VENDA) AS MES,
+    SUM(QTD_VENDA) AS TOTAL_VENDAS
+FROM 
+    `case-gb-elcio.vendas.tb_unificadas_sql`
+GROUP BY 
+    ANO, MES
+ORDER BY 
+    ANO, MES;
